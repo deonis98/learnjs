13014ffb45536baf7c5112acc78cd8b007759311
deonis98 <den98.82@mail.ru>
2020-04-08 20:35:22 +0300

    08.04.2020

--- scripts.js
+++ scripts.js
@@ -4,31 +4,40 @@
  *  Function Learn
  */
 
-function clunk(times) {
-	var num = times;
-	while (num > 0) {
-		display("clunk");
-		num = num - 1;
-	}
-}
+var products = ["Choo Choo Chocolate",
+	"Icy Mint",
+	"Cake Batter",
+	"Bubblegum"];
+var hasBubbleGum = [false, false, false, true];
 
-function thingamajig(size) {
-	var facky = 1;
-	clunkCounter = 0;
-	if (size == 0) {
-		display("clank");
-	} else if (size == 1) {
-		display("thunk");
-	} else {
-		while (size > 1) { facky = facky * size; size = size - 1; } clunk(facky);
-	}
-}
+for (var i = 0; i < hasBubbleGum.length; i++) {
 
-function display(output) {
+	if (hasBubbleGum[i]) console.log(products[i] + " contains bubble gum");
+
+}
+console.log('============================');
+
+var scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
+var output;
+var highScore = 0;
+
+for (var i = 0; i < scores.length; i = i + 1) {
+	output = "Bubble solution #" + i + " score: " + scores[i];
 	console.log(output);
-	clunkCounter = clunkCounter + 1;
+
+	if (scores[i] > highScore) highScore = scores[i];
+
+
 }
 
-var clunkCounter = 0;
-thingamajig(5);
-console.log(clunkCounter)
+console.log('Bubbles test:' + scores.length);
+console.log('Highest bubble score:' + highScore);
+
+var bestSolutions = []; 
+for (var i = 0; i < scores.length; i++) { 
+	if (scores[i] == highScore) { 
+		bestSolutions.push(i); 
+	} 
+} 
+console.log("Solutions with the highest score: " + bestSolutions);
+
